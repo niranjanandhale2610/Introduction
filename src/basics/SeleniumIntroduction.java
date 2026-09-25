2c6d561fdd4d557b9c9bd8d5ec3f6719b2021ffc
niranjanandhale2610 <andhaleniranjan@gmail.com>
2026-09-25 15:04:05 +0530

    Added login test case

--- src/basics/SeleniumIntroduction.java
+++ src/basics/SeleniumIntroduction.java
@@ -1,22 +1,31 @@
 package basics;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class SeleniumIntroduction {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
 		WebDriver driver = new ChromeDriver();
 		
-		driver.get("https://sauce-demo.myshopify.com/");
+		String name = "rahul";
+		driver.get("https://rahulshettyacademy.com/locatorspractice/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.manage().window().maximize();
-		driver.findElement(By.linkText("Log In")).click();
-		driver.findElement(By.id("customer_email")).sendKeys("user@sauce.com");
-		driver.findElement(By.cssSelector(".long.password")).sendKeys("12345");
-		driver.findElement(By.xpath("//input[@class='button']")).click();
-
+		driver.findElement(By.id("inputUsername")).sendKeys(name);
+		driver.findElement(By.name("inputPassword")).sendKeys("rahulshettyacademy");
+		driver.findElement(By.id("chkboxOne")).click();
+		driver.findElement(By.xpath("//button[contains(@class,'submit')]")).click();
+		Thread.sleep(2000);
+		System.out.println(driver.findElement(By.tagName("p")).getText());
+		Assert.assertEquals(driver.findElement(By.tagName("p")).getText(), "You are successfully logged in.");
+        
+        Assert.assertEquals(driver.findElement(By.xpath("//h2")).getText(), "Hello "+name+",");
 	}
 
 }
